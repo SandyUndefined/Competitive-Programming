292a46f64cfb0f0d307ce1debfab335459935873
Sandy_undefined <08sandysk@gmail.com>
2020-07-07 19:55:19 +0530

    done

---
+++ HackerRank/Algorithms/Implementation/Picking Numbers.py
@@ -0,0 +1,10 @@
+def pickingNumbers(a):
+    for i in range(0,len(a)):
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    pickingNumbers(a)
