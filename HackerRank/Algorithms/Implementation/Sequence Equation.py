b0f99bf03a0b0115ef038fabf91f3626eba55c23
Sandy_undefined <08sandysk@gmail.com>
2020-07-12 21:29:41 +0530

    done

--- HackerRank/Algorithms/Implementation/Sequence Equation.py
+++ HackerRank/Algorithms/Implementation/Sequence Equation.py
@@ -1,12 +1,11 @@
-from itertools import permutations
 def permutationEquation(p):
     maxi = max(p)
     mini = min(p)
-    per = permutations(p)
-    for i in list(p):
-        p[i] = i
-    # for j in range(1,n):
-    #     if j
+    a = []
+    for i in range(mini,maxi+1):
+        a.append(p.index(p.index(i)+1)+1)
+    for _ in a:
+        print(_)
 
 
 if __name__ == '__main__':
