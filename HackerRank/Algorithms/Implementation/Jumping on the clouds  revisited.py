cb34869809759c2fea6cb897dae868bb0163c920
Sandy_undefined <08sandysk@gmail.com>
2020-07-12 21:33:14 +0530

    done

---
+++ HackerRank/Algorithms/Implementation/Jumping on the clouds  revisited.py
@@ -0,0 +1,13 @@
+def jumpingOnClouds(c, k):
+
+
+if __name__ == '__main__':
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    jumpingOnClouds(c, k)
