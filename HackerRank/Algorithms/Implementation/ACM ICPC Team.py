06f0e6519497466874d7112d674f4cc2c6b8f540
Sandy_undefined <08sandysk@gmail.com>
2020-07-20 12:52:19 +0530

    Incomplete

---
+++ HackerRank/Algorithms/Implementation/ACM ICPC Team.py
@@ -0,0 +1,16 @@
+def acmTeam(topic):
+
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    topic = []
+
+    for _ in range(n):
+        topic_item = input()
+        topic.append(topic_item)
+
+     acmTeam(topic)
