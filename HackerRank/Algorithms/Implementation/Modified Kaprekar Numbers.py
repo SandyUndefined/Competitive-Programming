d0e11dee8addfa4faab22220012dfff8bc01de67
Sandy_undefined <08sandysk@gmail.com>
2020-07-21 15:55:16 +0530

    Incomplete

--- HackerRank/Algorithms/Implementation/Modified Kaprekar Numbers.py
+++ HackerRank/Algorithms/Implementation/Modified Kaprekar Numbers.py
@@ -1,5 +1,16 @@
+import math
+
+
 def kaprekarNumbers(p, q):
-    return
+    # c = list(str(int(math.pow(p,2))))
+    # print("Yes" if int(c[0])+int(c[1])==p else "NO")
+    for i in range(p,q+1):
+        c = list(str(int(math.pow(i,2))))
+        # print(c)
+        if len(c)>1:
+            if int(c[0]) + int(c[1]) == i:
+                print(i)
+
 
 if __name__ == '__main__':
     p = int(input())
